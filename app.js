b2869ae68db5429e473913b665919d3ad4e5071e
devEMEL <dominichenry882@gmail.com>
2020-04-07 19:48:12 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// let inputValue = document.getElementsByClassName("inputValue");
+
+// let inputDOM = Array.from(inputValue);
+// inputDOM.forEach(inpD => {
+//     inpD.addEventListener("click",e => {
+//         document.getElementById("howYouKnowUs").value = e.target.textContent;
+//     })
+// })
+
+
+// document.getElementById("showDropdown").addEventListener("click",e => {
+//     let dc = document.getElementById("dropdown-content");
+     //dc.style.display === 'none' ? dc.style.display = 'block' : dc.style.display = 'none';
+//     dc.classList.toggle("show")
+
+// })
+
+ // <a href="#" id="showDropdown">v</a>
+
+ 
